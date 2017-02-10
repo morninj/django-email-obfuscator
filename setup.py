a7c576dc0f6d091764be923f45052862f6ed2e81
Joe Mornin <joe@mornin.org>
2017-02-10 13:45:54 -0800

    Bump to v0.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-email-obfuscator',
-    version='0.1.4',
+    version='0.1.5',
     description='A Django template filter to protect email addresses from \
     crawlers.',
     author='Joseph Mornin',
